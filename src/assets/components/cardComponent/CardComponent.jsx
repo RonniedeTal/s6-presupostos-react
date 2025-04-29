39d181c88295cf289730a2b090b355c7693e7db2
unknown <ronnievelz@gmail.com>
2025-04-29 21:40:26 +0200

    extra commit

--- src/assets/components/cardComponent/CardComponent.jsx
+++ src/assets/components/cardComponent/CardComponent.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CardComponent.css';
 
 function CardComponent(props) {
+  const [check, setCheck]=useState(true)
+  const handleCheckOnchange=(e)=>{
+    // setCheck(!check)
+    // const value=e.target.value
+  if(e.target.value){
+    setCheck(!check)
+    console.log(props.h4Content, check);
+    
+  }
+    
+  }
+
   return (
     <div className="card w-50 mt-3 mx-auto shadow-lg">
       <div className="card-body inner-card">
@@ -10,6 +22,7 @@ function CardComponent(props) {
           <p>{props.pContent}</p>
         </div>
         <h3 className="price">{props.priceContent}$</h3>
+        <span> <input type="checkbox" value={check} name={props.h4Content} onChange={handleCheckOnchange}/>Add</span>
       </div>
     </div>
   );
