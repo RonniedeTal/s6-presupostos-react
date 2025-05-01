1a338fd91ac72e8da57f1f341df851ddaead99c2
RonniedeTal <ronnievelz@gmail.com>
2025-05-01 10:42:53 +0200

    creating a json data

--- src/assets/section/budgetsChooseSection/BudgetSelection.jsx
+++ src/assets/section/budgetsChooseSection/BudgetSelection.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './BudgetSelection.css';
 import CardComponent from '../../components/cardComponent/CardComponent';
+import dataJson from "../../../Data.json"
+
+
+
 
 function BudgetSelection() {
+  const [data, setData]=useState([dataJson])
+
+console.log(data);
+
+
   return (
     <div className="budget-selection ">
-      <CardComponent  
-        h4Content='Seo'
-        pContent='Programing a complete responsive web'
-        priceContent="300"
-      />
-      <CardComponent 
-        h4Content='Ads'
-        pContent='Programing a complete responsive web'
-        priceContent="400"
-        
-      />
-      <CardComponent 
-        h4Content='Web'
-        pContent='Programing a complete responsive web'
-        priceContent="500"
-      />
-
-      <h4 className='total mt-5'>Total: </h4>
+      {dataJson.map((eachData)=>{
+      return (
+         <CardComponent key={eachData.id} 
+      h4Content={eachData.service}
+      pContent={eachData.content}
+      priceContent={eachData.priceContent}
+    /> 
+      )
+      })}
+     
+     
+      <h4 className='total mt-5'>Total: </h4> 
     </div>
   );
 }
